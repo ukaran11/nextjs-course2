3e1dfa96156588db974676e8b9c74d2b4855f3b6
Utsav Karan <ukaran11@gmail.com>
2021-10-19 12:34:28 +0530

    Utsav | Added allEvents

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../dummy-data";
 
+
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
   return (
